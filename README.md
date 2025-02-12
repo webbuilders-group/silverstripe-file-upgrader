50609283bfe2c84d9b145133efc809688ce39dbc
UndefinedOffset <echipman@webbuildersgroup.com>
2025-02-12 12:35:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Silverstripe File Upgrader
+=================
+Provides tasks for upgrading the assets folder from Silverstripe 3 installs for Silverstripe 5+, this is a port of code from Silverstripe 4 that was removed in Silverstripe 5
+
+## Maintainer Contact
+* Ed Chipman ([UndefinedOffset](https://github.com/UndefinedOffset))
+
+## Requirements
+* Silverstripe Assets 2.0+
+
+
+## Installation
+__Composer (recommended):__
+```
+composer require webbuilders-group/silverstripe-file-upgrader
+```
---
+++ src/Tasks/VersionedFilesMigrationTask.php
@@ -0,0 +1,36 @@
+<?php
+namespace WebbuildersGroup\FileUpgrader\Tasks;
+
+use WebbuildersGroup\FileUpgrader\VersionedFilesMigrator;
+use SilverStripe\Dev\BuildTask;
+
+class VersionedFilesMigrationTask extends BuildTask
+{
+    const STRATEGY_DELETE = 'delete';
+
+    const STRATEGY_PROTECT = 'protect';
+
+    private static $segment = 'migrate-versionedfiles';
+
+    protected $title = 'Migrate versionedfiles';
+
+    protected $description = 'If you had the symbiote/silverstripe-versionedfiles module installed on your 3.x site, it
+        is no longer needed in 4.x as this functionality is provided by default. This task will remove the old _versions
+        folders or protect them, depending on the strategy you use. Use ?strategy=delete or ?strategy=protect (Apache
+        only). [Default: delete]';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $strategy = $request->getVar('strategy') ?: self::STRATEGY_DELETE;
+        $migrator = VersionedFilesMigrator::create($strategy, ASSETS_PATH, true);
+        $migrator->migrate();
+    }
+}
